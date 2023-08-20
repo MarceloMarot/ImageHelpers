282ba0d17fe1dd8c81cd5f1e9ee246d6431c3583
Marcelo Marot Rosas <marotmarcelo@gmail.com>
2023-08-20 03:23:26 -0300

    Flet - paso atras, checkboxes en columna unica

--- checkbox_var.py
+++ checkbox_var.py
@@ -3,6 +3,7 @@ import flet as ft
 
 def main(page: ft.Page):
 
+    # Estilos 
     page.theme = ft.Theme(
         scrollbar_theme=ft.ScrollbarTheme(
             track_color={
@@ -30,87 +31,78 @@ def main(page: ft.Page):
     page.window_height=1000
     # page.bgcolor=ft.colors.LIME_ACCENT_100
     # page.bgcolor=ft.color.INDIGO_100
-    def button_clicked(e):
+    def submit_opciones(e):
         valores=""
-        # for i in range(0,len(c)):
-        #     if  c[i].value:
-        #         valores += " " + c[i].label   
-        #         # reinicio de switches
-        #         c[i].value = False
-    # for i in range(0,len(columna)):
-    #     if  columna[i].value:
-    #         valores += " " + columna[i].label   
-    #         # reinicio de switches
-    #         columna[i].value = False            
-
-
+        for i in range(0,len(checkboxes)):
+            if  checkboxes[i].value:
+                valores += " " + checkboxes[i].label   
+                # reinicio de switches
+                checkboxes[i].value = False
         t.value = (
             f"Switch values are:  {valores}."
         )
         page.update()
 
+
+    def checkboxes_ninguno(e):
+        for i in range(0,len(checkboxes)):
+            # resetear de switches
+            checkboxes[i].value = False
+        page.update()
+
+
+    def checkboxes_todos(e):
+        for i in range(0,len(checkboxes)):
+            # setear de switches
+            checkboxes[i].value = True
+        page.update()
+        
+
+
+
     t = ft.Text(size=30 ,width=400, height=200, bgcolor=ft.colors.AMBER_200, )
 
-    c=[]
+    checkboxes=[]
     for i  in range(0,20):
-        ccc = ft.Switch(label=f"{i}", value=False )
-        c.append(ccc) 
+        ccc = ft.Switch(label=f"{i}", value=False)
+        checkboxes.append(ccc) 
         # ft.Switch(label=f"{i}", value=False)
 
-    d=[]
-    for i  in range(20,35):
-        ddd = ft.Switch(label=f"{i}", value=False )
-        d.append(ddd) 
-        # ft.Switch(label=f"{i}", value=False)
+    boton_submit = ft.ElevatedButton(text="Submit", on_click=submit_opciones, bgcolor="red",color="white")
+    b2 = ft.ElevatedButton(text="Todo", on_click=checkboxes_todos   ,width=150)
+    b3 = ft.ElevatedButton(text="Nada", on_click=checkboxes_ninguno ,width=150)
 
 
+    fila_botones = ft.Row(
+        wrap=False,
+        # spacing=10,       # espaciado horizontal entre contenedores
+        spacing=50,       # espaciado horizontal entre contenedores
+        run_spacing=50,     # espaciado vertical entre filas
+        controls = [ b2, b3],
+        # width=page.window_width,
+        # width=float("inf"), # anchura de columna    
+        width=400, # anchura de columna   
+        # height=600,         # altura de columna
+        scroll=ft.ScrollMode.ALWAYS,
+    )
 
-    b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
-    page.add(t,  b)
+
+    page.add(t,  fila_botones)
     # Columna de checkboxes
-    columna=[]
-    col = ft.Column(
+    columna = ft.Column(
         wrap=False,
         # spacing=10,       # espaciado horizontal entre contenedores
         spacing=10,       # espaciado horizontal entre contenedores
         run_spacing=50,     # espaciado vertical entre filas
-        controls=c,
+        controls=checkboxes,
         # width=page.window_width,
         # width=float("inf"), # anchura de columna    
-        width=300, # anchura de columna   
+        width=400, # anchura de columna   
         height=600,         # altura de columna
         scroll=ft.ScrollMode.ALWAYS,
     )
-    columna.append(col)
-    col = ft.Column(
-        wrap=False,
-        # spacing=10,       # espaciado horizontal entre contenedores
-        spacing=10,       # espaciado horizontal entre contenedores
-        run_spacing=50,     # espaciado vertical entre filas
-        controls=d,
-        # width=page.window_width,
-        # width=float("inf"), # anchura de columna    
-        width=300, # anchura de columna   
-        height=600,         # altura de columna
-        scroll=ft.ScrollMode.ALWAYS,
-    )
-    columna.append(col)
-    fila=ft.Row(
-        wrap=False,
-        # spacing=10,       # espaciado horizontal entre contenedores
-        spacing=10,       # espaciado horizontal entre contenedores
-        run_spacing=50,     # espaciado vertical entre filas
-        controls=columna,
-        # width=page.window_width,
-        # width=float("inf"), # anchura de columna    
-        width=700,      # anchura de fila  
-        height=600,     # altura de fila
-    )
-    page.add(fila)
-
-
-
-
+    page.add(columna)
+    page.add(boton_submit)
 
 
 #   b = ft.ElevatedButton("Click HIER!", on_click=boton_clickeado, data=0, bgcolor="green", width=150)
