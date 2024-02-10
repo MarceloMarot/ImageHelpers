d2cff528714144fab7888d655ecebd018a3974f5
Marcelo Marot <marotmarcelo@gmail.com>
2024-02-10 17:47:45 -0300

    Primera version compilable

---
+++ instalar.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# creacion directorio para entorno virtual
+py -m venv virtual_env/
+
+# activacion entorno virtual
+source virtual_env/bin/activate 
+
+# actualizar PIP
+pip install --upgrade pip
+
+# instalacion desde archivo
+pip  install -r requirements.txt
+ 
